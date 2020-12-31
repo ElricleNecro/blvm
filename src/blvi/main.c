ff1123321d04ee339c2eda03ebfa39c47ad18701
Guillaume Plum <guiplum@gmail.com>
2020-12-31 13:38:24 +0100

    Changed option name to be clearer.

--- src/blvi/main.c
+++ src/blvi/main.c
@@ -10,7 +10,7 @@ int main(int argc, const char *argv[]) {
 	Args_Error err;
 
 	int limit = -1;
-	Args_Add(args, "-m", "--max", T_INT, &limit, "Maximum number of instruction to run.");
+	Args_Add(args, "-l", "--limit", T_INT, &limit, "Maximum number of instruction to run.");
 
 	err = Args_Parse(args, argc, argv);
 	if( err == TREAT_ERROR ) {
