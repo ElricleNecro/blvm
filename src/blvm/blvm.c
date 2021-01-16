2d6b5ae4962acdf32ee67571e3bed98989512fc0
Guillaume Plum <guiplum@gmail.com>
2021-01-16 16:25:49 +0100

    feat(gt): implemented gt instruction.

--- src/blvm/blvm.c
+++ src/blvm/blvm.c
@@ -242,6 +242,15 @@ Trap blvm_execute_inst(Blvm *bl) {
 			bl->ip += 1;
 			break;
 
+		case INST_GT:
+			if( bl->sp < 2 )
+				return TRAP_STACK_UNDERFLOW;
+
+			bl->stack[bl->sp - 2].u64 = bl->stack[bl->sp - 1].u64 > bl->stack[bl->sp - 2].u64;
+			bl->sp -= 1;
+			bl->ip += 1;
+			break;
+
 		case INST_HALT:
 			bl->halt = true;
 			bl->ip += 1;
