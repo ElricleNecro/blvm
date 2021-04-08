466906ca91127af8e68ee137a2e5fc4bf8148f06
Guillaume Plum <guiplum@gmail.com>
2021-04-08 08:16:50 +0200

    feat(build): better handling of files output.

--- test-x86.sh
+++ test-x86.sh
@@ -2,6 +2,13 @@
 
 set -e
 
-blasm2nasm -I lib $1 > $(basename $1).asm
-nasm -g -wall -felf64 $(basename $1).asm
-ld -o $(basename ${1%.*}) $(basename $1).o
+OUTDIR=build/Debug
+BINDIR="${OUTDIR}/examples"
+OBJDIR="build/obj/Debug/asm"
+
+mkdir -p "${BINDIR}"
+mkdir -p "${OBJDIR}"
+
+blasm2nasm -I lib $1 > ${OUTDIR}/$(basename $1).asm
+nasm -g -wall -felf64 ${OUTDIR}/$(basename $1).asm -o ${OBJDIR}/$(basename $1).o
+ld -o ${BINDIR}/$(basename ${1%.*}) ${OBJDIR}/$(basename $1).o
